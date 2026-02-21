d401163e9813cba7fbb04818e16ea71afff7567c
Himanshu Bhirud <116067299+W47K3RJO1N@users.noreply.github.com>
2026-02-21 22:34:12 +0530

    Add hollow number pyramid pattern generator

---
+++ Patterns/hollownumberpyramid.py
@@ -0,0 +1,8 @@
+n = 4
+for i in range(1, n + 1):
+    for j in range(1, 2*n):
+        if i == n or j == n-i+1 or j == n+i-1:
+            print("1", end="")
+        else:
+            print(" ", end="")
+    print()
